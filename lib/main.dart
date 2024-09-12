c861d70707252bcdb19574e21102c5bedc74e207
Ahmed <ahmedabdelbar231@gmail.com>
2024-09-12 09:25:57 +0300

    creating my project by dart

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  print("enter your name :");
+  String name = stdin.readLineSync()!;
+  print("enter your age :");
+  int age = int.parse(stdin.readLineSync()!);
+  print("enter the item you want to sale :");
+  String item = stdin.readLineSync()!;
+  print("enter the salary of this item :");
+  double salary = double.parse(stdin.readLineSync()!);
+  print("you sure you want to sale it :");
+  bool decision = bool.parse(stdin.readLineSync()!);
+  print(
+      "the user name is $name and his age is $age \nand the item he want to sale is $item and the salary of item is $salary \nand his decision is $decision");
+}
