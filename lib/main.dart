11ebd8ea0470a175f9023cce07860f366ce18df8
Ahmed <ahmedabdelbar231@gmail.com>
2024-09-12 09:47:38 +0300

    ahemd

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 
 void main() {
-  print("enter your name :");
-  String name = stdin.readLineSync()!;
   print("enter your age :");
   int age = int.parse(stdin.readLineSync()!);
   print("enter the item you want to sale :");
@@ -12,5 +10,5 @@ void main() {
   print("you sure you want to sale it :");
   bool decision = bool.parse(stdin.readLineSync()!);
   print(
-      "the user name is $name and his age is $age \nand the item he want to sale is $item and the salary of item is $salary \nand his decision is $decision");
+      "the user name is and his age is $age \nand the item he want to sale is $item and the salary of item is $salary \nand his decision is $decision");
 }
