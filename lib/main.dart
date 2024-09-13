ac5a6ad20a49ea97080222b5963a8b4e4f944748
Ahmed <ahmedabdelbar231@gmail.com>
2024-09-13 11:29:57 +0300

    new section

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
 import 'dart:io';
 
 void main() {
-  print("enter your name :");
+  print("Enter your name :");
   String name = stdin.readLineSync()!;
-  print("enter your age :");
+  
+  print("Enter your age :");
   int age = int.parse(stdin.readLineSync()!);
-  print("enter the item you want to sale :");
-  String item = stdin.readLineSync()!;
-  print("enter the salary of this item :");
-  double salary = double.parse(stdin.readLineSync()!);
-  print("you sure you want to sale it :");
-  bool decision = bool.parse(stdin.readLineSync()!);
+  
+  print("Enter your country :");
+  String country = stdin.readLineSync()!;
+  
+  print("Enter your department :");
+  String department = stdin.readLineSync()!;
+  
   print(
-      "the user name is $name and his age is $age \nand the item he want to sale is $item and the salary of item is $salary \nand his decision is $decision");
+      "Your data is :\n   The name is : $name \n   The age is : $age \n   The country is : $country \n   The department is : $department \n ");
+  
+  if (department == "soft" || department == "bio") {
+    print("The price of the section is 14,000");
+  } else if (department == "general") {
+    print("The price of the section is 1000");
+  } else {
+    print("This is an incorrect value !");
+  }
 }
