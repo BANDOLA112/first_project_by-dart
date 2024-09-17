626909e1c0a0c4f2d8a354b392c13f2844c45dc0
Ahmed <ahmedabdelbar231@gmail.com>
2024-09-17 12:05:11 +0300

    ahemd

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,34 @@
 import 'dart:io';
 
 void main() {
+  String selectedDepartment = "";
+  int price = 0;
   print("Enter your name :");
   String name = stdin.readLineSync()!;
-  
+
   print("Enter your age :");
   int age = int.parse(stdin.readLineSync()!);
-  
+
   print("Enter your country :");
   String country = stdin.readLineSync()!;
-  
-  print("Enter your department :");
-  String department = stdin.readLineSync()!;
-  
-  print(
-      "Your data is :\n   The name is : $name \n   The age is : $age \n   The country is : $country \n   The department is : $department \n ");
-  
-  if (department == "soft" || department == "bio") {
-    print("The price of the section is 14,000");
-  } else if (department == "general") {
-    print("The price of the section is 1000");
-  } else {
-    print("This is an incorrect value !");
+
+  print("select your department from :\n 1-bio \n 2-soft \n 3-general");
+  int department = int.parse(stdin.readLineSync()!);
+  switch (department) {
+    case 1:
+      selectedDepartment = "bio";
+      price=14000;
+      break;
+    case 2:
+      selectedDepartment = "soft";
+      price = 14000;
+      break;
+    case 3:
+      selectedDepartment = "general";
+      price = 1000;
+      break;
   }
+
+  print(
+      "Your data is :\n   The name is : $name \n   The age is : $age \n   The country is : $country \n   The department is : $selectedDepartment \n   The price of the section is :$price");
 }
